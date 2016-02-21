636df2c660b0bbd1f69fadd0ac7ef4e2682cac52
Jack Thorp <jackthorp13@gmail.com>
2016-02-21 15:27:13 +0000

    Added failing test

--- app/test/test.js
+++ app/test/test.js
@@ -1,9 +1,22 @@
 var assert = require("assert");
+var request = require("supertest");
+var app = require("../index.js");
 
 describe('Array', function(){
   describe('indexOf()', function(){
     it('should return -1 when the value is not present', function(){
       assert.equal(-1, [1,2,3].indexOf(4));
     });
+  });
+});
+
+describe('About Page', function(){
+  it('should be available', function() {
+    request(app)
+      .get('/about')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+      });
   });
 });
